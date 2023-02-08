033cc9a4ae24f683c96ec6198f6f81bdbb31ba51
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-02-08 17:21:59 +0000

    Carousel Bug Fix

--- src/Component/Home/Slider.js
+++ src/Component/Home/Slider.js
@@ -1,33 +1,39 @@
-import React, {  useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import MovieContext from "../../Context/MovieContext";
-import {FcPrevious,FcNext} from "react-icons/fc"
+import { FcPrevious, FcNext } from "react-icons/fc";
 import "./Slider.css";
 
 const Banner = () => {
-const movieData = useContext(MovieContext)
+  const movieData = useContext(MovieContext);
 
-const [currentIndex, setCurrentIndex] = useState(0);
- 
- const handlePrev = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
     const prevIndex = currentIndex - 1;
-    setCurrentIndex(prevIndex < 0 ? movieData.trending.results.length : prevIndex);
+    setCurrentIndex(
+      prevIndex < 0 ? movieData.trending.results.length - 1 : prevIndex
+    );
   };
   const handleNext = () => {
     const nextIndex = currentIndex + 1;
-    setCurrentIndex(nextIndex === movieData.trending.results.length ? 0 : nextIndex);
+    setCurrentIndex(
+      nextIndex === movieData.trending.results.length ? 0 : nextIndex
+    );
   };
 
   return (
     <div className="slider-banner">
-      <img src={`https://image.tmdb.org/t/p/original/${movieData.trending.results[currentIndex].poster_path} `}  />
-      <p style={{zIndex:"99"}}>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${movieData.trending.results[currentIndex].poster_path} `}
+      />
+      <p style={{ zIndex: "99" }}>
         {movieData.trending.results[currentIndex].overview}
       </p>
       <button className="prev-btn" onClick={handlePrev}>
-        <FcPrevious/>
+        <FcPrevious />
       </button>
       <button className="next-btn" onClick={handleNext}>
-       <FcNext />
+        <FcNext />
       </button>
     </div>
   );
